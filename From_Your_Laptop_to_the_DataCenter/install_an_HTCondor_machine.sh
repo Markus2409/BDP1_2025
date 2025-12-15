790c2ba27ee6ddeb15248e1266422e916a3f6d3b
Marco Cuscunà <marco.cuscuna01@gmail.com>
2025-12-15 18:02:07 +0100

    Update install_an_HTCondor_machine.sh

--- From_Your_Laptop_to_the_DataCenter/install_an_HTCondor_machine.sh
+++ From_Your_Laptop_to_the_DataCenter/install_an_HTCondor_machine.sh
@@ -1,62 +1,77 @@
-HTCondor User's Manual Quick Start Guide: https://htcondor.readthedocs.io/en/latest/users-manual/quick-start-guide.html#users-quick-start-guide
-HTCondor Administration Quick Start Guide: https://htcondor.readthedocs.io/en/latest/getting-htcondor/admin-quick-start.html
+================================================================================
+SPIEGAZIONE CONFIGURAZIONE CLUSTER HTCONDOR SU AWS
+================================================================================
 
-EXERCISE:
+Questa esercitazione guida alla creazione di un cluster HTCondor composto da 3 nodi 
+distinti: un Central Manager (il "cervello"), un Submit Node (il punto di accesso) 
+e un Execute Node (il lavoratore).
 
-create 3 VMs on AWS:
-1) central manager t3_small
-2) submit node t3_small
-3) execute node t3_medium
+--- 1. PREPARAZIONE INFRASTRUTTURA (AWS & NFS) ---
 
-##########################################################################
-(make available the /data2 directory to all the VMs, i.e. USE NFS!!)
-##########################################################################
+- create 3 VMs on AWS
+  Si creano tre istanze EC2. 
+  - Central Manager e Submit Node usano 't3_small' (bastano per la gestione).
+  - Execute Node usa 't3_medium' perché richiede più potenza di calcolo (CPU/RAM) 
+    per eseguire i job scientifici.
 
-#INSTALL THE CENTRAL MANAGER
-#LOGIN VIA SSH to the central manager
+- (make available the /data2 directory to all the VMs, i.e. USE NFS!!)
+  È fondamentale che tutti i nodi vedano gli stessi file. Si deve configurare 
+  NFS (come visto nell'esercizio precedente) per condividere la cartella '/data2' 
+  tra le macchine.
 
-curl -fsSL https://get.htcondor.org | sudo GET_HTCONDOR_PASSWORD="BDP1_2025" /bin/bash -s -- --no-dry-run --central-manager <the_central_manager_private_IP>
-#CHECK CONDOR STATUS
-condor_status
-ps auxwf
+--- 2. INSTALLAZIONE HTCONDOR (Script get.htcondor.org) ---
 
-#INSTALL THE SUBMIT NODE
-#LOGIN VIA SSH to the submit node
-curl -fsSL https://get.htcondor.org | sudo GET_HTCONDOR_PASSWORD="BDP1_2025" /bin/bash -s -- --no-dry-run --submit <the_central_manager_private_IP>
-condor_status
+Lo script utilizzato scarica e installa automaticamente HTCondor configurando i ruoli.
 
-#INSTALL THE EXECUTE NODE
-#LOGIN VIA SSH to the execute node
-curl -fsSL https://get.htcondor.org | sudo GET_HTCONDOR_PASSWORD="BDP1_2025" /bin/bash -s -- --no-dry-run --execute <the_central_manager_private_IP>
-condor_status
+A. CENTRAL MANAGER
+   curl -fsSL https://get.htcondor.org | sudo GET_HTCONDOR_PASSWORD="BDP1_2025" /bin/bash -s -- --no-dry-run --central-manager <IP>
+   - Scarica lo script di installazione ufficiale.
+   - Imposta la password di sicurezza del cluster ("BDP1_2025").
+   - Il flag '--central-manager' configura questa macchina come orchestratore del cluster.
+   - <IP> deve essere l'indirizzo IP privato della macchina stessa.
 
+B. SUBMIT NODE
+   curl ... --submit <the_central_manager_private_IP>
+   - Il flag '--submit' configura questa macchina per permettere agli utenti di sottomettere i job.
+   - È necessario specificare l'IP del Central Manager affinché il nodo sappia a chi rivolgersi.
 
-############################################
-ADVACED: CHANGE THE SLOT TYPE : https://htcondor.readthedocs.io/en/latest/admin-manual/ep-policy-configuration.html
-[root@execute ~]# cat /etc/condor/config.d/01-execute.config
-CONDOR_HOST = <the_central_manager_private_IP>
-# For details, run condor_config_val use role:get_htcondor_execute
-use role:get_htcondor_execute
-use FEATURE: StaticSlots
+C. EXECUTE NODE
+   curl ... --execute <the_central_manager_private_IP>
+   - Il flag '--execute' configura questa macchina come Worker Node (dove girano i job).
+   - Si collega al Central Manager specificato.
 
-SLOT_TYPE_1 = cpus=2
-SLOT_TYPE_2 = cpus=1
-NUM_SLOTS_TYPE_1 = 1
-NUM_SLOTS_TYPE_2 = 2
+--- 3. VERIFICA ---
 
-[root@execute ~]#condor_reconfig
-[root@execute ~]#systemctl restart condor
-[root@execute ~]#condor_status
+- condor_status
+  Mostra lo stato del cluster. Se tutto funziona, dovresti vedere l'Execute Node elencato 
+  come risorsa disponibile.
 
-############################################
+- ps auxwf
+  Visualizza i processi attivi ad albero. Utile per vedere i demoni di Condor (condor_master, 
+  condor_collector, condor_negotiator, ecc.) in esecuzione.
 
-###################################  HINT BELOW ####################
+--- 4. CONFIGURAZIONE AVANZATA (Slot Types) ---
 
- Security Group must allow tcp for ports 0 - 65535 from the same security group, i.e.:
- All TCP    TCP      0 - 65535     sg-008742ba0467986fe (aws_condor)
-Security group must allow ping from the same security group, i.e.:
- All    ICMP-IPv4   All    N/A     sg-008742ba0467986fe (aws_condor)
-Security group must allow ssh on port 22 from everywhere as ususal
+HTCondor divide le risorse (CPU) in "Slot". Di default crea uno slot per ogni core. 
+Questa configurazione personalizza come le risorse vengono divise.
 
-#you can also allow all traffic (all protocols, all ports) from the same security group
-#######################################################################
+- /etc/condor/config.d/01-execute.config
+  File di configurazione creato sull'Execute Node.
+  - use FEATURE: StaticSlots: Indica di usare slot statici invece che dinamici (partitionable).
+  - SLOT_TYPE_1 = cpus=2, NUM_SLOTS_TYPE_1 = 1: Crea 1 slot che consuma 2 CPU.
+  - SLOT_TYPE_2 = cpus=1, NUM_SLOTS_TYPE_2 = 2: Crea 2 slot che consumano 1 CPU ciascuno.
+  
+  In totale, su una macchina con 4 vCPU, avremo 3 slot disponibili con capacità diverse.
+
+- condor_reconfig / systemctl restart condor
+  Ricarica la configurazione per applicare le modifiche agli slot.
+
+--- 5. SICUREZZA DI RETE (AWS Security Groups) ---
+
+- All TCP 0 - 65535 from same security group
+  HTCondor usa porte dinamiche per comunicare tra i vari demoni. È essenziale aprire 
+  TUTTE le porte TCP tra le macchine che fanno parte dello stesso Security Group 
+  (tra di loro si devono fidare ciecamente).
+
+- All ICMP-IPv4
+  Permette il ping tra le macchine per diagnostica.
